380f01b8bf510fd13380699712bfb2ea4245a0de
roeeiit1 <roeeiit1@gmail.com>
2016-12-11 10:24:21 +0200

    added comments and changes the things functions get

--- Functions.py
+++ Functions.py
@@ -9,29 +9,61 @@ from Resources.MapObject import MapObject
 from Resources.GameObject import GameObject
 
 
-def get_danger_level(location):
+def get_danger_level(locations,id_aircraft):
+    locations[0]
     pass
 
 
 def get_enemy_pirates_in_range(randius, location):
+    """
+
+    :param randius:
+    :param location:
+    :type location:iLocaton
+    :return: int
+    """
     pass
 
 
-def get_number_per_island(game, id_island, id_aircraft, id_city):
+def get_number_per_island(game, island_object, aircraft_object, city_object):
+    """
+
+    :param game:
+    :type game:PirateGame
+    :param island_object:
+    :type island_object:GameObject
+    :param aircraft_object:
+    :type aircraft_object:GameObject
+    :param city_object:
+    :type city_object:GameObject
+    :return:
+    """
+
+
     number = 100
-    number = number - id_aircraft.distance(id_island)
-    number = number - game.distance(id_city, id_island)
-    number = number - (get_enemy_pirates_in_range(2, id_island) * 10)
-    if get_enemy_pirates_in_range(2, id_island) >= 3:
+    number = number - aircraft_object.distance(aircraft_object)
+    number = number - city_object.distance(island_object)
+    number = number - (get_enemy_pirates_in_range(2, island_object.get_location) * 10)
+    if get_enemy_pirates_in_range(2, island_object) >= 3:
         return -100
     return number
     pass
 
 
-def get_list_priority(game, id_aircraft, id_city):
-    l = [get_number_per_island(game, game.get_all_islands[0], id_aircraft, id_city),
-         get_number_per_island(game, game.get_all_islands[1], id_aircraft, id_city),
-         get_number_per_island(game, game.get_all_islands[2], id_aircraft, id_city),
-         get_number_per_island(game, game.get_all_islands[3], id_aircraft, id_city)]
+def get_list_priority(game, aircraft_object, city_object):
+    """
+     :param game:
+     :type game:PirateGame
+     :param aircraft_object:
+     :type aircraft_object:GameObject
+     :param city_object:
+     :type city_object:GameObject
+     :return: List
+     """
+    l = [get_number_per_island(game, game.get_all_islands[0], aircraft_object, city_object),
+         get_number_per_island(game, game.get_all_islands[1], aircraft_object, city_object),
+         get_number_per_island(game, game.get_all_islands[2], aircraft_object, city_object),
+         get_number_per_island(game, game.get_all_islands[3], aircraft_object, city_object)]
     return l
     pass
+
--- MyBot.py
+++ MyBot.py
@@ -1,3 +1,3 @@
 def do_turn(game):
-
+    boolsShipTakenTurn=[False,False,False,False,False]
     pass
