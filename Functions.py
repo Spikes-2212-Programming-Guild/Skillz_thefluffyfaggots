6f651b316b40a39d72863af889a0e089e60f98f8
roeeiit1 <roeeiit1@gmail.com>
2016-12-11 11:43:35 +0200

    added get_friendly

--- Functions.py
+++ Functions.py
@@ -7,6 +7,7 @@ from Resources.LocationClass import Location
 from Resources.Aircraft import Aircraft
 from Resources.MapObject import MapObject
 from Resources.GameObject import GameObject
+from Resources.Pirates import PirateGame
 
 
 def get_danger_level(locations, id_aircraft):
@@ -23,6 +24,24 @@ def get_enemy_pirates_in_range(randius, location):
     :return: int
     """
     pass
+
+def get_friendly_pirates_in_range(game,radius, location):
+    """
+    :param game:
+    :type game:PirateGame
+    :param radius:
+    :type radius:int
+    :param location:
+    :type location:Location
+    :return: list
+    """
+    friends_in_range = []
+    for aircraft in game.get_my_living_pirates():
+        if aircraft.distance(location) < radius:
+            friends_in_range.append(aircraft)
+
+    return friends_in_range
+
 
 
 def get_number_per_island(game, island_object, aircraft_object, city_object):
@@ -42,7 +61,7 @@ def get_number_per_island(game, island_object, aircraft_object, city_object):
     number = 100
     number -= aircraft_object.distance(aircraft_object)
     number -= city_object.distance(island_object)
-    number -= (len(get_enemy_pirates_in_range(2, island_object.get_location)) * 10)
+    number -= ((len(get_enemy_pirates_in_range(2, island_object.get_location))-(len(get_friendly_pirates_in_range(game,2,island_object.get_location())))) * 10)
     if len(get_enemy_pirates_in_range(2, island_object)) >= 3:
         return -100
     return number
@@ -58,8 +77,11 @@ def get_list_priority(game, aircraft_object, city_object):
      :type city_object:GameObject
      :return: List
      """
+    game.get_attack_range()
     l = [get_number_per_island(game, game.get_all_islands[0], aircraft_object, city_object),
          get_number_per_island(game, game.get_all_islands[1], aircraft_object, city_object),
          get_number_per_island(game, game.get_all_islands[2], aircraft_object, city_object),
          get_number_per_island(game, game.get_all_islands[3], aircraft_object, city_object)]
     return l
+
+
--- MyBot.py
+++ MyBot.py
@@ -1,3 +1,58 @@
+from game import *
+
+
+def get_closest_island(game):
+    """
+
+        :param game:
+        :type game: PirateGame
+        :type mycity: City
+        :return: None
+    """
+    islands = game.get_all_islands()
+    myCity = game.get_my_cities()[0]
+    dist = myCity.distance(islands[0])
+    result = islands[0]
+    for island in islands:
+        if myCity.distance(island) < dist:
+            result = island
+    return result
+
+
+def init_data(game):
+    """
+            :param game:
+            :type game: PirateGame
+            :return: None
+    """
+
+
 def do_turn(game):
-    boolsShipTakenTurn=[False,False,False,False,False]
-    pass
+    """
+
+    :param game:
+    :type game: PirateGame
+    :return: None
+    """
+    bools_Ship_Taken_turn = [False, False, False, False, False]
+    pirateIndex = 0
+    for pirate in game.get_my_living_pirates():
+        for enemy in game.get_enemy_living_aircrafts():
+            if pirate.in_attack_range(enemy):
+                bools_Ship_Taken_turn[pirateIndex] = True
+                game.attack(pirate, enemy)
+            else:
+                bools_Ship_Taken_turn[pirateIndex] = False
+        pirateIndex += 1
+    island = get_closest_island(game)
+
+    pirateIndex = 0
+    for pirate in game.get_my_living_pirates():
+        if not bools_Ship_Taken_turn[pirateIndex]:
+            sailOption = game.get_sail_options(pirate, island.get_location())[0]
+            game.set_sail(pirate, sailOption)
+        pirateIndex += 1
+
+    for drone in game.get_my_living_drones():
+        sailOption = game.get_sail_options(drone, game.get_my_cities()[0].get_location())[0]
+        game.set_sail(drone, sailOption)
