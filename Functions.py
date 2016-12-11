c1841cddc259f8dfa835b80db898143fce9a4957
roeeiit1 <roeeiit1@gmail.com>
2016-12-11 09:50:32 +0200

    added Function.get_list_priority

--- Functions.py
+++ Functions.py
@@ -17,13 +17,21 @@ def get_enemy_pirates_in_range(randius, location):
     pass
 
 
-def get_number_per_island(game, id_island,id_aircraft,id_city):
+def get_number_per_island(game, id_island, id_aircraft, id_city):
     number = 100
     number = number - id_aircraft.distance(id_island)
     number = number - game.distance(id_city, id_island)
     number = number - (get_enemy_pirates_in_range(2, id_island) * 10)
-    if get_enemy_pirates_in_range(2, id_island):
-        return 0
+    if get_enemy_pirates_in_range(2, id_island) >= 3:
+        return -100
     return number
     pass
 
+
+def get_list_priority(game, id_aircraft, id_city):
+    l = [get_number_per_island(game, game.get_all_islands[0], id_aircraft, id_city),
+         get_number_per_island(game, game.get_all_islands[1], id_aircraft, id_city),
+         get_number_per_island(game, game.get_all_islands[2], id_aircraft, id_city),
+         get_number_per_island(game, game.get_all_islands[3], id_aircraft, id_city)]
+    return l
+    pass
