b1f7e831a0cbc43908bea63cc9c01217c38517d8
roeeiit1 <roeeiit1@gmail.com>
2016-12-11 10:27:07 +0200

    fixed get_number_per_island

--- Functions.py
+++ Functions.py
@@ -9,7 +9,7 @@ from Resources.MapObject import MapObject
 from Resources.GameObject import GameObject
 
 
-def get_danger_level(locations,id_aircraft):
+def get_danger_level(locations, id_aircraft):
     locations[0]
     pass
 
@@ -39,11 +39,10 @@ def get_number_per_island(game, island_object, aircraft_object, city_object):
     :return:
     """
 
-
     number = 100
     number = number - aircraft_object.distance(aircraft_object)
     number = number - city_object.distance(island_object)
-    number = number - (get_enemy_pirates_in_range(2, island_object.get_location) * 10)
+    number = number - (len(get_enemy_pirates_in_range(2, island_object.get_location)) * 10)
     if get_enemy_pirates_in_range(2, island_object) >= 3:
         return -100
     return number
@@ -66,4 +65,3 @@ def get_list_priority(game, aircraft_object, city_object):
          get_number_per_island(game, game.get_all_islands[3], aircraft_object, city_object)]
     return l
     pass
-
